c804a1ba1926a1db3e6853da2ea850ac0c760e28
it's nosehad <itznosehad@gmail.com>
2023-02-25 19:59:35 +0100

    Update main.sh to fit with new translation

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-gcc Main.c gfs.c Sortieralgorithmen/InsertionSort.c Sortieralgorithmen/BubbleSort.c Sortieralgorithmen/MergeSort.c Sortieralgorithmen/QuickSort.c -w -g -o algorithmen && ./algorithmen
+gcc Main.c gfs.c Implementations/InsertionSort.c Implementations/BubbleSort.c Implementations/MergeSort.c Implementations/QuickSort.c -w -g -o algorithmen && ./algorithmen
